04fc9014f65755dc760a4a6e4405df6d2929bef3
De <godde@lowes.com>
2023-01-10 19:00:24 +0530

    Initial Commit

---
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
+import './CourseGoalList.css';
+
+const CourseGoalList = props => {
+  return (
+    <ul className="goal-list">
+      <div className='goallistheader'>Goal List:</div>
+      {props.items.map(goal => (
+        <CourseGoalItem
+          key={goal.id}
+          id={goal.id}
+          onDelete={props.onDeleteItem}
+        >
+          {goal.text}
+        </CourseGoalItem>
+      ))}
+    </ul>
+  );
+};
+
+export default CourseGoalList;
---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+import styled from "styled-components";
+
+const FormControl = styled.div`
+  
+    margin: 0.5rem 0;
+  
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background: rgb(238, 218, 218);
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredValue);
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <FormControl className={!isValid && 'invalid'}>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+      </FormControl>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
